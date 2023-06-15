c7056351bb6e084616aed84cbf287aae28470326
john <ralphesleyer@gmail.com>
2023-06-15 19:24:52 +0800

    added MobileScreen and DesktopScreen widgets and make the screen responsive for mobile and desktop

---
+++ lib/screens/DesktopScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DesktopScreen extends StatefulWidget {
+  @override
+  _DesktopScreenState createState() => _DesktopScreenState();
+}
+
+class _DesktopScreenState extends State<DesktopScreen> {
+  @override
+  Widget build(BuildContext contex) {
+    return Scaffold(
+      body: Container(
+        color: Colors.green,
+      ),
+    );
+  }
+}
---
+++ lib/screens/MobileScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MobileScreen extends StatefulWidget {
+  @override
+  _MobileScreenState createState() => _MobileScreenState();
+}
+
+class _MobileScreenState extends State<MobileScreen> {
+  @override
+  Widget build(BuildContext contex) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
