30dae97d3e2690a4ddab4f088f853110f5893a0c
JohnEsleyer <ralphesleyer@gmail.com>
2023-08-11 10:16:08 +0800

    ADD: added string to flashcard

--- lib/widgets/markdown_widget.dart
+++ lib/widgets/markdown_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MarkdownWidget extends StatefulWidget {
   final String markdown;
 
@@ -12,6 +11,8 @@ class MarkdownWidget extends StatefulWidget {
 
 class _MarkdownWidgetState extends State<MarkdownWidget> {
   List<Widget> _rendered = [];
+  List<List<Widget>> _flashcardWidgets = [[]];
+  bool _isFlashcard = false;
 
   @override
   void initState() {
@@ -26,93 +27,206 @@ class _MarkdownWidgetState extends State<MarkdownWidget> {
       print(line);
       if (line.startsWith('# ')) {
         // Header
-        _rendered.add(
-          Text(
-            line.substring(2),
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              color: Colors.white,
-            ),
-          ),
-        );
-      } else if (line.startsWith('## ')) {
-        // Subheader
-        _rendered.add(
-          Text(
-            line.substring(3),
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontWeight: FontWeight.bold,
-              fontSize: 26,
-              color: Colors.white,
-            ),
-          ),
-        );
-      } else if (line.startsWith('### ')) {
-        // Subheader 2
-        _rendered.add(
-          Text(
-            line.substring(4),
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontWeight: FontWeight.bold,
-              fontSize: 22,
-              color: Colors.white,
-            ),
-          ),
-        );
-      } else if (line.startsWith('- ')) {
-        // Bullet
-        _rendered.add(Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8),
-              child: Icon(
-                Icons.circle,
-                size: 10,
+        if (!_isFlashcard) {
+          _rendered.add(
+            Text(
+              line.substring(2),
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
                 color: Colors.white,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8),
-              child: Text(
-                line.substring(2),
-                style: TextStyle(
-                  decoration: TextDecoration.none,
+          );
+        } else {
+          _flashcardWidgets[_flashcardWidgets.length > 0
+                  ? _flashcardWidgets.length - 1
+                  : 0]
+              .add(
+            Text(
+              line.substring(2),
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+          );
+        }
+      } else if (line.startsWith('## ')) {
+        // Subheader
 
+        if (!_isFlashcard) {
+          _rendered.add(
+            Text(
+              line.substring(3),
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: Colors.white,
+              ),
+            ),
+          );
+        } else {
+          _flashcardWidgets[_flashcardWidgets.length > 0
+                  ? _flashcardWidgets.length - 1
+                  : 0]
+              .add(
+            Text(
+              line.substring(3),
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: Colors.black,
+              ),
+            ),
+          );
+        }
+      } else if (line.startsWith('### ')) {
+        // Subheader 2
+        if (!_isFlashcard) {
+          _rendered.add(
+            Text(
+              line.substring(4),
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+                color: Colors.white,
+              ),
+            ),
+          );
+        } else {
+          _flashcardWidgets[_flashcardWidgets.length > 0
+                  ? _flashcardWidgets.length - 1
+                  : 0]
+              .add(
+            Text(
+              line.substring(4),
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+                color: Colors.black,
+              ),
+            ),
+          );
+        }
+      } else if (line.startsWith('- ')) {
+        // Bullet
+
+        if (!_isFlashcard) {
+          _rendered.add(Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Icon(
+                  Icons.circle,
+                  size: 10,
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Text(
+                  line.substring(2),
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ));
+        } else {
+          _flashcardWidgets[_flashcardWidgets.length > 0
+                  ? _flashcardWidgets.length - 1
+                  : 0]
+              .add(Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Icon(
+                  Icons.circle,
+                  size: 10,
+                  color: Colors.black,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Text(
+                  line.substring(2),
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ));
+        }
+      } else if (line.startsWith('**') && line.endsWith('**')) {
+        // Bold
+        if (!_isFlashcard) {
+          _rendered.add(Text(
+            line.substring(2, line.length - 2),
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ));
+        } else {
+          _flashcardWidgets[_flashcardWidgets.length > 0
+                  ? _flashcardWidgets.length - 1
+                  : 0]
+              .add(Text(
+            line.substring(2, line.length - 2),
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 16,
+            ),
+          ));
+        }
+      } else if (line.startsWith('_') && line.endsWith('_')) {
+        // Italic
+        if (!_isFlashcard) {
+          _rendered.add(Text(
+            line.substring(1, line.length - 1),
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontStyle: FontStyle.italic,
               fontWeight: FontWeight.normal,
               color: Colors.white,
               fontSize: 16,
-                ),
-              ),
             ),
-          ],
-        ));
-      } else if (line.startsWith('**') && line.endsWith('**')) {
-        // Bold
-        _rendered.add(Text(
-          line.substring(2, line.length - 2),
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            fontWeight: FontWeight.bold,
-              color: Colors.white,
+          ));
+        } else {
+          _flashcardWidgets[_flashcardWidgets.length > 0
+                  ? _flashcardWidgets.length - 1
+                  : 0]
+              .add(Text(
+            line.substring(1, line.length - 1),
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.normal,
+              color: Colors.black,
               fontSize: 16,
-          ),
-        ));
-      } else if (line.startsWith('_') && line.endsWith('_')) {
-        // Italic
-        _rendered.add(Text(
-          line.substring(1, line.length - 1),
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.normal,
-              color: Colors.white,
-              fontSize: 16,
-          ),
-        ));
+            ),
+          ));
+        }
       } else if (line.startsWith('![') && line.endsWith(')')) {
         // Image with URL
         RegExp regex = RegExp(r'!\[.*\]\((.*?)\)');
@@ -120,40 +234,97 @@ class _MarkdownWidgetState extends State<MarkdownWidget> {
         if (match != null && match.groupCount == 1) {
           String imageUrl = match.group(1) ?? '';
           if (imageUrl.isNotEmpty) {
-            _rendered.add(Center(
-              child: Container(
-                height: 300,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    imageUrl,
-                   
-                    
+            if (!_isFlashcard) {
+              _rendered.add(Center(
+                child: Container(
+                  height: 300,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      imageUrl,
+                    ),
                   ),
                 ),
-              ),
-            ));
+              ));
+            } else {
+              _flashcardWidgets[_flashcardWidgets.length > 0
+                      ? _flashcardWidgets.length - 1
+                      : 0]
+                  .add(Center(
+                child: Container(
+                  height: 300,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      imageUrl,
+                    ),
+                  ),
+                ),
+              ));
+            }
           }
         }
-      } else {
+      } else if (line.startsWith('!{') && !_isFlashcard) {
+        _isFlashcard = true;
+      } else if (line.startsWith('}!') && _isFlashcard) {
+        _isFlashcard = false;
+
+        
+
         _rendered.add(
-          Text(
-            line,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontWeight: FontWeight.normal,
+          Container(
+            decoration: BoxDecoration(
               color: Colors.white,
-              fontSize: 16,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: _flashcardWidgets[_flashcardWidgets.length > 0
+                  ? _flashcardWidgets.length - 1
+                  : 0],
             ),
           ),
         );
+
+        _flashcardWidgets.add([]); // Add empty array
+
+        // _flashcardWidgets.clear();
+      } else {
+        // Normal text
+        if (!_isFlashcard) {
+          _rendered.add(
+            Text(
+              line,
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+          );
+        } else {
+          _flashcardWidgets[_flashcardWidgets.length > 0
+                  ? _flashcardWidgets.length - 1
+                  : 0]
+              .add(
+            Text(
+              line,
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+                fontSize: 16,
+              ),
+            ),
+          );
+        }
       }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-   
+    print(_flashcardWidgets);
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
